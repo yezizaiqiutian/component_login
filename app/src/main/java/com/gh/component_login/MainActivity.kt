3b59c68a4652082095805640a2c2edaa7efa5923
gaohui01 <gaohui01@5i5j.com>
2022-03-24 13:28:53 +0800

    引入core库

--- app/src/main/java/com/gh/component_login/MainActivity.kt
+++ app/src/main/java/com/gh/component_login/MainActivity.kt
@@ -2,10 +2,12 @@ package com.gh.component_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gh.lib_core.utils.LogUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LogUtils.d("login组件测试组件引用")
     }
 }
